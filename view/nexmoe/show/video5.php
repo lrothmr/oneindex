c0c85183213fa6a000bfa14e1efe428edf6aff2d
lrothmr <61190265+lrothmr@users.noreply.github.com>
2020-04-10 20:08:50 +0800

    Update video5.php

--- view/nexmoe/show/video5.php
+++ view/nexmoe/show/video5.php
@@ -1,15 +1,22 @@
 <?php view::layout('layout')?>
-
+ 
 <?php 
+//仅支持教育版和企业版
+if(strpos($item['downloadUrl'],"sharepoint.com") == false){
+	header('Location: '.$item['downloadUrl']);exit();
+}
 $item['thumb'] = onedrive::thumbnail($item['path']);
+$mpd =  str_replace("thumbnail","videomanifest",$item['thumb'])."&part=index&format=dash&useScf=True&pretranscode=0&transcodeahead=0";
 ?>
-
+ 
 <?php view::begin('content');?>
+<link class="dplayer-css" rel="stylesheet" href="https://cdn.jsdelivr.net/npm/dplayer/dist/DPlayer.min.css">
+<script src="https://cdn.jsdelivr.net/npm/dashjs/dist/dash.all.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/dplayer/dist/DPlayer.min.js"></script>
+<script src="//pan.520648.xyz/view/js/subtitles-octopus.js"></script>
 <div class="mdui-container-fluid">
 	<div class="nexmoe-item">
-	<video class="mdui-video-fluid mdui-center" preload controls poster="<?php @e($item['thumb']);?>">
-	  <source src="<?php e($item['downloadUrl']);?>" type="video/mp4">
-	</video>
+		<div class="mdui-center" id="dplayer"></div>
 	<!-- 固定标签 -->
 	<div class="mdui-textfield">
 	  <label class="mdui-textfield-label">下载地址</label>
@@ -19,7 +26,45 @@ $item['thumb'] = onedrive::thumbnail($item['path']);
 	  <label class="mdui-textfield-label">引用地址</label>
 	  <textarea class="mdui-textfield-input"><video><source src="<?php e($url);?>" type="video/mp4"></video></textarea>
 	</div>
-	</div>
 </div>
+<script>
+const dp = new DPlayer({
+	container: document.getElementById('dplayer'),
+	lang:'zh-cn',
+	video: {
+	    url: '<?php e($item['downloadUrl']);?>',
+	    pic: '<?php @e($item['thumb']);?>',
+	    type: 'auto'
+	},
+      subtitle: {
+        url: '<?php $urlparts = pathinfo($url); e($urlparts['dirname'].'/'.$urlparts['filename'].'.vtt');?>',
+        type: 'webvtt',
+        fontSize: '15px',
+        bottom: '5%',
+        color: '#ffffff'
+    }
+});
+dp.on('canplay',function() {
+          fetch('', {
+       redirect: 'follow'   }).then(function(response) {
+return response.text();
+}).then(function(text) {
+var video = document.getElementsByTagName('video')[0];
+window.SubtitlesOctopusOnLoad = function() {
+var options = {
+video: video,
+// subUrl: '',
+subContent: text,
+fonts: ["//gapis.geekzu.org/g-fonts/ea/notosanssc/v1/NotoSansSC-Regular.otf", "//gapis.geekzu.org/g-fonts/ea/notosanstc/v1/NotoSansTC-Regular.otf", "//gapis.geekzu.org/g-fonts/ea/notosansjapanese/v6/NotoSansJP-Regular.otf"],
+workerUrl: '/subtitles-octopus-worker.js'
+};
+window.octopusInstance = new SubtitlesOctopus(options);
+};
+if (SubtitlesOctopus) {
+SubtitlesOctopusOnLoad();
+}
+});
+});
+</script>
 <a href="<?php e($url);?>" class="mdui-fab mdui-fab-fixed mdui-ripple mdui-color-theme-accent"><i class="mdui-icon material-icons">file_download</i></a>
 <?php view::end('content');?>
